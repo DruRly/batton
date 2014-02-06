ee71099774da1717030b2386ed9baa70d6ee7430
Dru Riley <drurly@gmail.com>
2014-02-06 12:51:40 -0500

    Share asynchronous beforeEach

--- spec/battonSpec.js
+++ spec/battonSpec.js
@@ -6,6 +6,20 @@ var url = 'http://lh3.ggpht.com/zjnnvZZdGfB6zDiluJzoP44dgxn-s_5IzAQpRvhPa1rGpLi2
 describe('Batton', function() {
   describe('POST /upload', function() {
     describe('upload is successful', function(){
+      var response;
+
+      beforeEach(function(done) {
+        superagent.post('http://localhost:5000/upload')
+        .send({
+          url: url,
+          name: 'test.png'
+        })
+        .end(function(error, res) {
+          response = res;
+          done();
+        });
+      });
+
       afterEach(function() {
         fs.unlink('./public/images/test.png', function(err) {
           if (err) throw err;
@@ -13,31 +27,14 @@ describe('Batton', function() {
         })
       });
 
-      it('saves an image to the public/images directory', function(done) {
-        superagent.post('http://localhost:5000/upload')
-        .send({
-          url: url,
-          name: 'test.png'
-        })
-        .end(function(error, res) {
-          fs.exists('./public/images/test.png', function(exists) {
-            expect(res.statusCode).toBe(200);
-            expect(exists).toBe(true);
-            done();
-          });
+      it('saves an image to the public/images directory', function() {
+        fs.exists('./public/images/test.png', function(exists) {
+          expect(exists).toBe(true);
         });
       });
 
-      it('returns the remote location of the image', function(done){
-        superagent.post('http://localhost:5000/upload')
-        .send({
-          url: url,
-          name: 'test.png'
-        })
-        .end(function(error, res) {
-          expect(res.text).toBe('http://localhost:5000/public/images/test.png');
-          done();
-        });
+      it('returns the remote location of the image', function(){
+        expect(response.text).toBe('http://localhost:5000/public/images/test.png');
       });
 
     })
